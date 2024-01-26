74b6524e47d5dbf9b6da257cdcf5a773ca9049be
trinhlehoangminh2000 <trinhlehoangminh2000@gmail.com>
2024-01-26 14:25:45 +0100

    Add experiment with L3 loss

---
+++ dense_weight.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.stats import gaussian_kde
+
+class DenseWeight:
+    def __init__(self, alpha=0.5, epsilon=1e-4):
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.min = None
+        self.max = None
+        self.mean_weight = None
+        self.kde = None
+
+    def fit(self, labels):
+        # Create a gaussian Kernel Density Estimate
+        self.kde = gaussian_kde(labels)
+
+        # Evaluate the KDE for each point in labels
+        pdf_values = self.kde(labels)
+        self.min = np.min(pdf_values)
+        self.max = np.max(pdf_values)
+
+        # Calculate initial weights
+        weights = self._weight_value(labels)
+
+        # Set mean weight
+        self.mean_weight = np.mean(weights)
+
+    def _normalize_kde(self, labels):
+        # Normalize KDE values
+        normalized_pdf = (self.kde(labels) - self.min) / (self.max - self.min)
+        return normalized_pdf
+
+    def _weight_value(self, labels):
+        # Calculate weight values
+        normalized_pdf = self._normalize_kde(labels)
+        weighted_values = np.maximum(1 - self.alpha * normalized_pdf, self.epsilon)
+        return weighted_values
+
+    def dense_weight(self, labels):
+        # Apply dense weight normalization
+        weights = self._weight_value(labels)
+        dense_weights = weights / self.mean_weight
+        return dense_weights
--- weighted_L3_loss.py
+++ weighted_L3_loss.py
@@ -1,18 +1,23 @@
 import torch
 import torch.nn.functional as F
 
-class WeightedMSELoss(torch.nn.modules.loss._Loss):
+class WeightedL3Loss(torch.nn.modules.loss._Loss):
     def __init__(self, dense_weight_model, size_average=None, reduce=None, reduction='mean'):
-        super(WeightedMSELoss, self).__init__(size_average, reduce, reduction)
+        super(WeightedL3Loss, self).__init__(size_average, reduce, reduction)
         self.dense_weight_model = dense_weight_model
 
     def forward(self, input, target):
         weights = torch.tensor(self.dense_weight_model.dense_weight(target.cpu().numpy()), dtype=torch.float32, device=input.device)
         mse_loss = F.mse_loss(input, target, reduction='none')
-        weighted_mse_loss = weights * mse_loss
+
+        with torch.no_grad():
+            mae_loss = F.l1_loss(input, target, reduction= 'none')
+
+        weighted_l3_loss = weights * mse_loss * mae_loss
+        
         if self.reduction == 'mean':
-            return torch.mean(weighted_mse_loss)
+            return torch.mean(weighted_l3_loss)
         elif self.reduction == 'sum':
-            return torch.sum(weighted_mse_loss)
+            return torch.sum(weighted_l3_loss)
         else:
-            return weighted_mse_loss
+            return weighted_l3_loss
