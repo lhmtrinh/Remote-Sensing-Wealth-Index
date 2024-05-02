9ba76a42c7813a88aeb05d164c4b9e81ac278e61
trinhlehoangminh2000 <trinhlehoangminh2000@gmail.com>
2024-05-02 03:02:14 +0200

    Update training script

---
+++ metrics/balanced_MAE.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+min_label = 0.28095343708992004
+max_label = 3.094515085220337
+
+def balanced_MAE(true, pred, num_bins=6):
+    true = np.array(true)
+    pred = np.array(pred)
+
+    # Define bin edges
+    bin_edges = np.linspace(min_label, max_label, num= num_bins+1)
+    bin_edges = bin_edges[1:num_bins]
+    # Digitize the true labels into bins
+    true_label_bins = np.digitize(true, bin_edges, right=True)
+
+    # Initialize variables to store the weighted MAE
+    mae = 0 
+
+    # Calculate the MAE for each bin and weight it by the inverse of the bin's count
+    for i in range(1, len(bin_edges)):  # Bin indices start at 1
+        bin_mask = (true_label_bins == i)  # Mask to select items in the current bin
+        
+        current_labels = true[bin_mask]
+        current_predictions = pred[bin_mask]
+        mae += np.mean(np.abs(current_labels - current_predictions))
+
+    return mae/num_bins
